76de65e166c05de043b1244f4cc7f65fb6919a47
Uragami Taichi <backpaper0@gmail.com>
2023-12-10 06:39:19 +0900

    Update

--- src/main/java/io/github/backpaper0/example/hello/Hello.java
+++ src/main/java/io/github/backpaper0/example/hello/Hello.java
@@ -6,6 +6,23 @@ package io.github.backpaper0.example.hello;
  */
 public class Hello {
     /**
+     * Greeting phrase template
+     */
+    private final String template;
+    /**
+     * Instantiate.
+     */
+    public Hello() {
+        this("Hello, %s!");
+    }
+    /**
+     * Instantiate.
+     * @param template Greeting phrase template
+     */
+    public Hello(String template) {
+        this.template = template;
+    }
+    /**
      * Say hello.
      * @return message
      */
@@ -18,7 +35,7 @@ public class Hello {
      * @return message
      */
     public String greeting(String name) {
-        return "Hello, " + name + "!";
+        return template.formatted(name);
     }
 }
 
